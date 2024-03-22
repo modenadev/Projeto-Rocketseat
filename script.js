27f7b1fef0bbec978aa3bdc06b6f9905a374068c
modenadev <ruanm0dena@outlook.com>
2024-03-22 11:32:50 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  //pega a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir img
+  if (html.classList.contains("light")) {
+    //light mode
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //darkmode
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  if (html.classList.contains("light")) {
+    // se a classe "light" estiver presente no elemento html, a página está no modo claro
+    img.setAttribute("alt", "UM CARA MUITO FODA DE OCULOS PRETO B)")
+  } else {
+    // se a classe "light" não estiver presente no elemento html, a página está no modo escuro
+    img.setAttribute("alt", "UM CARA FODA DE OCULOS DE GRAU B)")
+  }
+}
